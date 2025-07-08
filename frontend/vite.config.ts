10dc6a6503e715b6abd712c34ef5ed5195c71e0c
ryan huynh <ryanhuynh200604@gmail.com>
2025-07-07 23:11:02 -0700

    updated vite.config and build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "YummiBook",
+  base: "/YummiBook/",
   build: {
     outDir: "../docs",
     emptyOutDir: true
